9abc9513430a2b7280c7b8141e568f309b19fe06
nicco <nicco@DESKTOP-OSKIESP>
2023-09-08 01:33:28 +0200

    inputs sistemati

--- EA test 2/src/view/inputs/KeyboardInputs.java
+++ EA test 2/src/view/inputs/KeyboardInputs.java
@@ -22,77 +22,74 @@ public class KeyboardInputs implements KeyListener {
 	@Override
 	public void keyReleased(KeyEvent e) {
 		switch (Gamestate.state) {
-		case START_TITLE:
-			if(e.getKeyCode() == KeyEvent.VK_ENTER) 
-				view.getStart().skipTitle();
-			break;
-		case MAIN_MENU:
-			view.getMenu().keyReleased(e.getKeyCode());
-			break;
-		case SELECT_AVATAR:
-			view.getAvatarMenu().keyReleased(e.getKeyCode());
-			break;
-		case PLAYING:
-			handleKeyReleasedPlayState(e.getKeyCode(), e);
-			break;
-		case OPTIONS:
-			view.getOptions().keyReleased(e.getKeyCode());
-			break;
-		default:
-			break;
+			case START_TITLE:
+				if(e.getKeyCode() == KeyEvent.VK_ENTER) 
+					view.getStart().skipTitle();
+				break;
+			case MAIN_MENU:
+				view.getMenu().keyReleased(e.getKeyCode());
+				break;
+			case SELECT_AVATAR:
+				view.getAvatarMenu().keyReleased(e.getKeyCode());
+				break;
+			case PLAYING:
+				handleKeyReleasedPlayState(e);
+				break;
+			case OPTIONS:
+				view.getOptions().keyReleased(e.getKeyCode());
+				break;
+			default:
+				break;
 		}					
 	}
 	
-	private void handleKeyReleasedPlayState(int keyCode, KeyEvent e) {
-		switch(keyCode) {
-		case KeyEvent.VK_ENTER:
-			view.getController().getPlay().stopPlayerAttacking();
-			break;
-		case KeyEvent.VK_SPACE:
-			view.getController().getPlay().stopPlayerParring();
-			break;
-		case KeyEvent.VK_P:
-			view.getController().getPlay().stopPlayerThrowing();
-			break;
-		case KeyEvent.VK_E:
-			view.getController().getPlay().stopPlayerInteracting();
-			break;
-			
-		case KeyEvent.VK_A:
-			view.getController().getPlay().getPlayer().resetDirection(getActionKey(e));
-			break;
-		case KeyEvent.VK_W:
-			view.getController().getPlay().getPlayer().resetDirection(getActionKey(e));;
-			break;
-		case KeyEvent.VK_D:
-			view.getController().getPlay().getPlayer().resetDirection(getActionKey(e));;
-			break;
-		case KeyEvent.VK_S:
-			view.getController().getPlay().getPlayer().resetDirection(getActionKey(e));;
-			break;
+	private void handleKeyReleasedPlayState(KeyEvent e) {
+		switch(e.getKeyCode()) {
+			case KeyEvent.VK_ENTER:
+				view.getController().getPlay().stopPlayerAttacking();
+				break;
+			case KeyEvent.VK_SPACE:
+				view.getController().getPlay().stopPlayerParring();
+				break;
+			case KeyEvent.VK_P:
+				view.getController().getPlay().stopPlayerThrowing();
+				break;
+			case KeyEvent.VK_E:
+				view.getController().getPlay().stopPlayerInteracting();
+				break;	
+			default:
+				view.getController().getPlay().getPlayer().resetDirection(getActionAssociatedToKey(e));
+				break;
 		}
 	}
 	
 	
-	public int getActionKey(KeyEvent e) {
-		switch(e.getKeyCode()) {
-		case KeyEvent.VK_ENTER:
-			return EntityView.ATTACK;
-		case KeyEvent.VK_SPACE:
-			return EntityView.PARRY;
-		case KeyEvent.VK_P:
-			return EntityView.THROW;	
-		case KeyEvent.VK_A:
-			return EntityView.LEFT;
-		case KeyEvent.VK_D:
-			return EntityView.RIGHT;	
-		case KeyEvent.VK_W:
-			return EntityView.UP;	
-		case KeyEvent.VK_S:
-			return EntityView.DOWN;
-		default :
-			return 999;
-		}
+	public int getActionAssociatedToKey(KeyEvent e) {
+		
+			if(e.getKeyCode() == KeyEvent.VK_ENTER)
+				return EntityView.ATTACK;
+			
+			else if(e.getKeyCode() == KeyEvent.VK_SPACE)
+				return EntityView.PARRY;
+			
+			else if(e.getKeyCode() == KeyEvent.VK_P)
+				return EntityView.THROW;
+			
+			else if(e.getKeyCode() == KeyEvent.VK_A)
+				return EntityView.LEFT;
+			
+			else if(e.getKeyCode() == KeyEvent.VK_D)
+				return EntityView.RIGHT;	
+			
+			else if(e.getKeyCode() == KeyEvent.VK_W)
+				return EntityView.UP;	
+			
+			else if(e.getKeyCode() == KeyEvent.VK_S)
+				return EntityView.DOWN;
+			
+			else return -1;
+			
+		
 	}
 
 	@Override
@@ -102,47 +99,37 @@ public class KeyboardInputs implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		switch (Gamestate.state) {
-		case PLAYING:
-			handleKeypressedDuringPlayState(e.getKeyCode(), e);
-			break;
-		case PAUSE:
-			view.getPause().keyPressed(e.getKeyCode());
-			break;
-		default:
-			break;
+			case PLAYING:
+				handleKeypressedDuringPlayState(e);
+				break;
+			case PAUSE:
+				view.getPause().keyPressed(e.getKeyCode());
+				break;
+			default:
+				break;
 		}
 	}
 
-	private void handleKeypressedDuringPlayState(int keyCode, KeyEvent e) {
-		switch(keyCode) {
-		case KeyEvent.VK_ENTER:
-			view.getController().getPlay().startPlayerAttacking();
-			break;
-		case KeyEvent.VK_SPACE:
-			view.getController().getPlay().startPlayerParring();
-			break;
-		case KeyEvent.VK_ESCAPE:
-			view.getController().getPlay().goToPauseState();
-			break;
-		case KeyEvent.VK_P:
-			view.getController().getPlay().startPlayerThrowing();
-			break;
-		case KeyEvent.VK_E:
-			view.getController().getPlay().startPlayerInteract();
-			break;
-			
-		case KeyEvent.VK_A:
-			view.getController().getPlay().getPlayer().choiceDirection(getActionKey(e));
-			break;
-		case KeyEvent.VK_W:
-			view.getController().getPlay().getPlayer().choiceDirection(getActionKey(e));;
-			break;
-		case KeyEvent.VK_D:
-			view.getController().getPlay().getPlayer().choiceDirection(getActionKey(e));;
-			break;
-		case KeyEvent.VK_S:
-			view.getController().getPlay().getPlayer().choiceDirection(getActionKey(e));;
-			break;
+	private void handleKeypressedDuringPlayState(KeyEvent e) {
+		switch(e.getKeyCode()) {
+			case KeyEvent.VK_ENTER:
+				view.getController().getPlay().startPlayerAttacking();
+				break;
+			case KeyEvent.VK_SPACE:
+				view.getController().getPlay().startPlayerParring();
+				break;
+			case KeyEvent.VK_ESCAPE:
+				view.getController().getPlay().goToPauseState();
+				break;
+			case KeyEvent.VK_P:
+				view.getController().getPlay().startPlayerThrowing();
+				break;
+			case KeyEvent.VK_E:
+				view.getController().getPlay().startPlayerInteract();
+				break;	
+			default:
+				view.getController().getPlay().getPlayer().choiceDirection(getActionAssociatedToKey(e));
+				break;
 		}
 		
 	}
